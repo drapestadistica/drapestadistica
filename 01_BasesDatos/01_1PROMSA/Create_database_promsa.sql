7d9a518fb648e5a86c18b3c5841499f4fa9a3cf7
Nicolay <nicolay200413@gmail.com>
2025-01-17 09:18:57 -0500

    Se modifica estructura de la base de datos PROMSA Signed-off-by: Nicolay <nicolay200413@gmail.com>

---
+++ 01_BasesDatos/01_1PROMSA/Create_database_promsa.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4b6aa8c32f88992233241d4f2c423b4734c927f01f2ad1df60e42bd8920207cc
+size 7464
---
+++ 01_BasesDatos/01_1PROMSA/Insercion_datos.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf2e2537778f317b7aa7dfe50eb5755a5561b7c40f3d4a65c29e1f70829056b2
+size 885
